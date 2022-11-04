78bf543ddb757741b05ad7e8c9fbe5320473c180
Drew Miller <d.m.lespaul@gmail.com>
2022-11-04 09:52:55 -0700

    Initial Commit

---
+++ WatchWhat-IOS/WatchWhat_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWhat_IOSApp.swift
+//  WatchWhat-IOS
+//
+//  Created by Drew Miller on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchWhat_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
